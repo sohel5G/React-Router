c31be666750a20554dfafd32fca07e488ab9d3b0
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-09-19 21:28:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-5xl text-center'>Hello, This is tailwidn test environemnt</h1>
+      <button className="btn btn-warning">Warning</button>
+
+    </>
+  )
+}
+
+export default App
